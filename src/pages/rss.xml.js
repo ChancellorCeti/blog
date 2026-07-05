5f107f50e07442dd49e753e67f0aca98c43c9c55
Chancellor Ceti <exdestroyer48@gmail.com>
2026-07-05 00:18:35 -0500

    somewhat fixed RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,22 +2,16 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
 export async function GET(context) {
-  const posts = await getCollection('posts');
-  
-  const sortedPosts = posts.sort((a, b) => 
-    new Date(b.data.pubDate).getTime() - new Date(a.data.pubDate).getTime()
-  );
-
+  const blog = await getCollection('posts');
   return rss({
-    title: "Chancellor Ceti's Blog",
+    title: 'Chancellor Ceti’s Blog',
     description: "Chancellor Ceti's blog on physics, math, coding, film, literature, politics, and history—plus whatever else inspires him",
     site: context.site,
-    items: sortedPosts.map((post) => ({
+    items: blog.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      link: `/posts/${post.id}/`, 
+      link: `/blog/${post.id}/`,
     })),
   });
 }
-
