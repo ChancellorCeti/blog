83f8efb449a52f69de80cedf9f5e92af210c33b4
Chancellor Ceti <exdestroyer48@gmail.com>
2026-07-05 00:37:07 -0500

    fixed it now i think

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,11 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
-  const blog = await getCollection('posts');
   return rss({
-    title: 'Chancellor Ceti’s Blog',
+    title: "Chancellor Ceti's Blog",
     description: "Chancellor Ceti's blog on physics, math, coding, film, literature, politics, and history—plus whatever else inspires him",
     site: context.site,
-    items: blog.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      link: `/blog/${post.id}/`,
-    })),
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
   });
 }
